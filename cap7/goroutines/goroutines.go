356b532e0fa41e3929dcf3b5adec9ab54ddf5eea
Thalio-Toledo <thalio.dev@gmail.com>
2025-05-06 08:42:41 -0300

    cap7

---
+++ cap7/goroutines/goroutines.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func imprimir(n int) {
+	for i := 0; i < 3; i++ {
+		fmt.Printf("%d ", n)
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+func main() {
+	go imprimir(2)
+	imprimir(3)
+	fmt.Printf("Finalizando...")
+}
